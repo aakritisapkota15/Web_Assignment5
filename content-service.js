69eb0716a586b2d1d3f7015b448ea0711218aca9
aakritisapkota15 <aakritisapkota86@gmail.com>
2024-12-11 01:53:17 -0500

    AS5 P2

--- content-service.js
+++ content-service.js
@@ -1,137 +1,171 @@
-// Import the 'fs' module for interacting with the file system
-const fs = require("fs");
-const { parse } = require("path");
+const fs = require("fs"); // File system module
+const { parse } = require("path"); // Path module
 
-// Arrays to store categories and articles data loaded from JSON files
-let categories = [];
-let articles = [];
+// using pool function from pg library to connect with postgresql DB
+const { pool } = require("pg");
 
-// Function to initialize data by loading categories and articles from JSON files
-function initialize() {
-  return new Promise((resolve, reject) => {
-    // Read the categories data from categories.json file
-    fs.readFile("./data/categories.json", "utf8", (err, cat) => {
-      if (err) return reject(err); // Reject the promise if an error occurs during file read
-      categories = JSON.parse(cat); // Parse and store categories data
+// Create a pool object to connect to the database
+const pool = new pool({
+  connectionString:
+    "postgresql://neondb_owner:2PZkNQ8HXJCa@ep-rapid-fire-a51ospt2-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require",
+  ssl: { rejectUnauthorized: false },
+});
 
-      // Nested readFile for articles.json
-      // We nest the second file read inside the first because we want to ensure that categories.json
-      // is successfully read and parsed before moving on to articles.json.
-      // This way, we load both files sequentially and can handle any errors independently.
-      fs.readFile("./data/articles.json", "utf8", (err, art) => {
-        if (err) return reject(err); // Reject the promise if an error occurs during file read
-        articles = JSON.parse(art); // Parse and store articles data
+// Function define to query DB to get articles
+module.exports.getAllArticles = () => {
+  return pool
+    .query("SELECT * FROM articles")
+    .then((res) => res.row)
+    .catch((err) => Promise.reject(err || "Result not found in Database"));
+};
 
-        // We call resolve() only once, after both files have been successfully read and parsed.
-        // Calling resolve() here signifies that initialization is complete and both categories
-        // and articles data are ready for use. If we called resolve() earlier, it would
-        // prematurely indicate that initialization was complete before loading both files.
-        resolve();
-      });
-    });
-  });
-}
+// Function define to query DB to get categories
+module.exports.getCategories = () => {
+  return pool
+    .query("SELECT * FROM categories")
+    .then((res) => res.row)
+    .catch((err) => Promise.reject(err || "Result not found in Database"));
+};
 
 // Function to get category name by ID
-function getCategoryName(categoryId) {
-  const category = categories.find((cat) => cat.Id === categoryId);
-  return category ? category.name : "Unknown";
-}
+module.exports.getCategoryName = (categoryId) => {
+  return pool
+    .query(`SELECT name FROM categories WHERE id ${categoryId}`)
+    .then((res) => res.row)
+    .catch((err) =>
+      Promise.reject(
+        err || `Result not found in Database for categoryId: ${categoryId}`
+      )
+    );
 
-function addArticle(articleData) {
-  return new Promise((resolve, reject) => {
-    articleData.published = articleData.published ? true : false;
-    articleData.Id = articles.length + 1; // Set ID to the current length + 1
-    articles.push(articleData);
-    resolve(articleData);
-  });
-}
+  // const category = categories.find((cat) => cat.Id === categoryId);
+  // return category ? category.name : "Unknown";
+};
+// Add new Article to the database
+module.exports.addArticle = (articleData) => {
+  // Step 0: Data validation to check we have all parameters
+  if (
+    !articleData.title ||
+    !articleData.articleDate ||
+    !articleData.category ||
+    !articleData.content ||
+    !articleData.published
+  ) {
+    return Promise.reject("Missing required fields");
+  }
+
+  // step 1: Create query
+  const query = `INSERT INTO articles (title, articleDate, category, content, published) VALUES ($1, $2, $3, $4, $5) RETURNING *`;
+
+  // Step 2: Create value
+  const values = [
+    articleData.title,
+    articleData.articleDate,
+    articleData.category,
+    articleData.content,
+    articleData.published,
+  ];
+
+  //Step 3: Execute query
+
+  return pool
+    .query(query, values)
+    .then((res) => res.rows[0])
+    .catch((err) =>
+      Promise.reject(
+        err || "Error while inserting into database. Contact administration"
+      )
+    );
+
+  // return new Promise((resolve, reject) => {
+  //   articleData.published = articleData.published ? true : false;
+  //   articleData.Id = articles.length + 1; // Set ID to the current length + 1
+  //   articles.push(articleData);
+  //   resolve(articleData);
+  // });
+};
 
 // Function to get articles by category and include the category name
 function getArticlesByCategory(categoryId) {
-  return new Promise((resolve, reject) => {
-    const filteredArticles = articles.filter(
-      (article) => article.category === parseInt(categoryId)
+  return pool
+    .query(`SELECT * FROM articles WHERE category = $1`, [categoryId])
+    .then((res) => res.rows)
+    .catch((err) =>
+      Promise.reject(
+        err || `Result not found in Database for categoryId: ${categoryId}`
+      )
     );
-    if (filteredArticles.length > 0) {
-      // Add category name to each article
-      const articlesWithCategoryName = filteredArticles.map((article) => ({
-        ...article,
-        categoryName: getCategoryName(article.category), // Add category name
-      }));
-      resolve(articlesWithCategoryName);
-    } else {
-      reject("No results found");
-    }
-  });
-}
+  // return new Promise((resolve, reject) => {
 
-function getAllArticles() {
-  return Promise.resolve(
-    articles.map((article) => ({
-      ...article,
-      categoryName: getCategoryName(article.category), // Add categoryName based on categoryId
-    }))
-  );
+  //   const filteredArticles = articles.filter(
+  //     (article) => article.category === parseInt(categoryId)
+  //   );
+  //   if (filteredArticles.length > 0) {
+  //     // Add category name to each article
+  //     const articlesWithCategoryName = filteredArticles.map((article) => ({
+  //       ...article,
+  //       categoryName: getCategoryName(article.category), // Add category name
+  //     }));
+  //     resolve(articlesWithCategoryName);
+  //   } else {
+  //     reject("No results found");
+  //   }
+  // });
 }
 
 // Function to get articles by minimum date and add category name
-function getArticlesByMinDate(minDateStr) {
-  return new Promise((resolve, reject) => {
-    const minDate = new Date(minDateStr); // Convert the string to a Date object
-    const filteredArticles = articles.filter(
-      (article) => new Date(article.articleDate) >= minDate
+module.exports.getArticlesByMinDate = (minDateStr) => {
+  return pool
+    .query(`SELECT * FROM articles WHERE articleDate >= $1`, [minDateStr])
+    .then((res) => res.rows)
+    .catch((err) =>
+      Promise.reject(
+        err || `Result not found in Database for minDate: ${minDateStr}`
+      )
     );
 
-    if (filteredArticles.length > 0) {
-      // Add category name to each filtered article
-      const articlesWithCategoryName = filteredArticles.map((article) => ({
-        ...article,
-        categoryName: getCategoryName(article.category), // Add category name
-      }));
-      resolve(articlesWithCategoryName);
-    } else {
-      reject("No results found");
-    }
-  });
-}
-function getArticleById(Id) {
-  return new Promise((resolve, reject) => {
-    const foundArticle = articles.find((article) => article.Id == parseInt(Id));
-    if (foundArticle) resolve(foundArticle);
-    else reject("no result returned");
-  });
-}
+  // return new Promise((resolve, reject) => {
+  //   const minDate = new Date(minDateStr); // Convert the string to a Date object
+  //   const filteredArticles = articles.filter(
+  //     (article) => new Date(article.articleDate) >= minDate
+  //   );
 
-// Function to get only published articles by filtering the articles array
-function getPublishedArticles() {
-  return Promise.resolve(articles.filter((article) => article.published)); // Return only articles with `published: true`
-}
-
-// Function to get all categories
-function getCategories() {
-  return Promise.resolve(categories); // Return the categories array as a resolved promise
-}
-
-// Function to get all articles and add category name
-function getArticles() {
-  return Promise.resolve(
-    articles.map((article) => ({
-      ...article,
-      categoryName: getCategoryName(article.category), // Add categoryName based on categoryId
-    }))
-  );
-}
-
-// Export the functions as an object to make them available to other files
-module.exports = {
-  initialize,
-  getAllArticles,
-  getCategories,
-  getArticles,
-  addArticle,
-  getArticlesByCategory,
-  getArticlesByMinDate,
-  getArticleById,
-  getCategoryName,
+  //   if (filteredArticles.length > 0) {
+  //     // Add category name to each filtered article
+  //     const articlesWithCategoryName = filteredArticles.map((article) => ({
+  //       ...article,
+  //       categoryName: getCategoryName(article.category), // Add category name
+  //     }));
+  //     resolve(articlesWithCategoryName);
+  //   } else {
+  //     reject("No results found");
+  //   }
+  // });
 };
+
+// Function to get article by ID from database
+
+module.exports.getArticleById = (Id) => {
+  return pool
+    .query(`SELECT * FROM articles WHERE id ${Id}`)
+    .then((res) => res.rows[0])
+    .catch((err) =>
+      Promise.reject(err || `The id ${Id} is not found in the database`)
+    );
+  // return new Promise((resolve, reject) => {
+  //   const foundArticle = articles.find((article) => article.Id == parseInt(Id));
+  //   if (foundArticle) resolve(foundArticle);
+  //   else reject("no result returned");
+  // });
+};
+
+// Function get only published articles
+module.exports.getPublishedArticles = () => {
+  return pool
+    .query(`SELECT * FROM articles WHERE published = true`)
+    .then((res) => res.rows)
+    .catch((err) => Promise.reject(err || "No published articles found"));
+  //return Promise.resolve(articles.filter((article) => article.published)); // Return only articles with `published: true`
+};
+
+module.exports.pool = pool;
